4780f2e2c94cf2efe9565f381ec9445ec6755c46
draconivis <patrick.gasser@proton.me>
2024-07-13 08:58:16 +0200

    removed configs from home-manager i favor of traditional configs

--- home/patrick/p14s.nix
+++ home/patrick/p14s.nix
@@ -45,171 +45,43 @@
 
   # Add stuff for your user as you see fit:
   home.packages = with pkgs; [
+    antidote
     arc-icon-theme
     awscli2
     bat
     bruno
     capitaine-cursors
-    fwupd
     cargo
+    dash
     devbox
     direnv
-    networkmanagerapplet
     docker
     docker-compose
     du-dust
     firefox-devedition-bin
+    fwupd
     fzf
+    git
+    home-manager
     jetbrains.phpstorm
     lazygit
-    dash
     mariadb
+    neovim
+    networkmanagerapplet
     pinentry-rofi
-    unzip
     pure-prompt
     silicon
     slack
-    xplr
     slides
-    vscodium
     tmux
     tmuxPlugins.tmux-fzf
-    #insomnium-bin
+    unzip
+    vscodium
+    xplr
+    zed-editor
+    zoxide
+    zellij
   ];
-
-  programs = {
-    alacritty = {
-      enable = true;
-      settings = {
-        live_config_reload = true;
-        # import = ["~/.config/alacritty/catppuccin-macchiato.toml"];
-        import = ["~/.config/alacritty/tokyonight-moon.toml"];
-        font = {
-          # size = 10.0;
-          normal = {
-            family = "JetBrainsMonoNF";
-            style = "Regular";
-          };
-        };
-        window = {
-          dynamic_padding = true;
-          # startup_mode = "Maximized";
-          decorations = "None";
-        };
-        shell = {
-          args = ["new-session" "-A" "-s" "main"];
-          program = "tmux";
-        };
-      };
-    };
-    # autorandr = {
-    #   enable = true;
-    # }
-    neovim.enable = true;
-    home-manager.enable = true;
-    git.enable = true;
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-      config = {
-        hide_env_diff = true;
-      };
-    };
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-      options = [ "--cmd cd" ];
-    };
-    zsh = {
-      enable = true;
-      history = {
-        size = 10000;
-        path = "${config.xdg.dataHome}/zsh/history";
-        share = true;
-      };
-      enableCompletion = true;
-      # autosuggestions.enable = true;
-      # syntaxHighlighting.enable = true;
-      # historySubstringSearch.enable = true;
-      initExtraFirst = ''
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
-        if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
-          source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
-        fi
-      '';
-      initExtra = ''
-        #vagrant
-        function vup () {
-          cd ~/work/start/homestead
-            vagrant up
-            cd -
-        }
-        function vdown () {
-          cd ~/work/start/homestead
-            vagrant suspend
-            cd -
-        }
-        function vssh () {
-          cd ~/work/start/homestead
-            vagrant ssh
-            cd -
-        }
-        function vhalt () {
-          cd ~/work/start/homestead
-            vagrant halt
-            cd -
-        }
-
-        ## load p10k
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      '';
-      shellAliases = {
-        "ls"="lsd";
-        "ll"="ls -lh";
-        "la"="ls -lAh";
-        ".."="cd ..";
-        "..."="cd ../..";
-        "...."="cd ../../..";
-        "lg"="lazygit";
-        "bc"="bin/console";
-        "dbx"="devbox";
-      };
-      antidote = {
-        enable = true;
-        useFriendlyNames = true;
-        plugins = [
-          # completions
-          # "mattmc3/zephyr path:plugins/completion"
-          "zsh-users/zsh-completions"
-          #g-plane/pnpm-shell-completion
-          # regular plugins
-          "ael-code/zsh-colored-man-pages"
-          "davidde/git"
-
-          ## work
-          #mattberther/zsh-pyenv
-          #sptndc/phpenv.plugin.zsh
-          #lukechilds/zsh-nvm
-
-          # prompt
-          "romkatv/powerlevel10k"
-          # syntax highlighting
-          "zdharma-continuum/fast-syntax-highlighting"
-          # these plugins need to be loaded last
-          "zsh-users/zsh-autosuggestions"
-          "zsh-users/zsh-history-substring-search"
-        ];
-      };
-    };
-    # oh-my-posh = {
-    #   enable = true;
-    #   enableZshIntegration = true;
-    #   useTheme = "catppuccin_macchiato";
-    # };
-  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
